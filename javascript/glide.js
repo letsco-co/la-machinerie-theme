932f6fce78fe3171c61d96b973a2aa66f0ef1b52
Auriane <auriane.chay@letsco.co>
2024-08-23 11:39:53 +0200

    feat: responsive slider

--- javascript/glide.js
+++ javascript/glide.js
@@ -2,10 +2,16 @@ if (document.querySelector('#$SliderID')) {
     new Glide('#$SliderID', {
         type: 'carousel',
         startAt: 0,
-        perView: $PerView,
         keyboard: true,
         focusAt: 'center',
-        peek: 100,
         autoplay: 2000,
+        perView: $PerView,
+        peek: 100,
+        breakpoints: {
+            800: {
+                perView: 1,
+                peek: 0,
+            },
+        }
     }).mount()
 }
